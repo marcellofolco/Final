fd910a2250867d203b1e21a3ab8c2ae0985944ef
march_in_march <march_846@hotmail.com>
2019-04-14 15:46:52 -0400

    Uploaded Project

---
+++ Cybersmooth/Assets/_Scripts/Controller2D.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+    public LayerMask collisionMask;
+
+    const float skin = 0.015f;
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+    new BoxCollider2D collider;
+    RaycastOrigins raycastOrigins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+
+        CalculateraySpacing();
+    }
+
+    void VerticalCollisions(ref Vector3 velocity)
+    {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) * skin;
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+
+            if (hit)
+            {
+                velocity.y = (hit.distance - skin) * directionY;
+                rayLength = hit.distance;
+            }
+        }
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        UpdateRaycastOrigins();
+        VerticalCollisions(ref velocity);
+        transform.Translate(velocity);
+    }
+
+    void UpdateRaycastOrigins()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skin * -2);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    void CalculateraySpacing()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skin * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+
+}
---
+++ Cybersmooth/Assets/_Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour
+{
+    float gravity = -20;
+    Vector3 velocity;
+    Controller2D controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
